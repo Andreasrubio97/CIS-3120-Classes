54ae8a3e0f0bd09e7fb195ba92830863037234b0
Andreasrubio97 <aldoramirez0897@gmail.com>
2025-02-18 20:32:26 -0500

    Changed to Lions doing many activities (Aldo)

--- Animal.py
+++ Animal.py
@@ -1,7 +1,34 @@
 class Animal:
-    def __init__(self): 
-        print("hello")
+    def __init__(self, name, food, sleep, run, hunt, fight): 
+        self.__name = name
+        self.__food = food
+        self.__sleep = sleep
+        self.__run = run
+        self.__hunt = hunt
+        self.__fight = fight
+        print("These animals are", self.__name)
 
     def talk(self):
-        print("hi")
+        print("Lions roar")
+    
+    def eat_food(self):
+        print(self.__name, "eat", self.__food)
+    
+    def animal_sleep(self):
+        print(self.__name, "sleep at", self.__sleep)
+    
+    def animal_run(self):
+        print(self.__name, "run", self.__run)
+
+    def animal_hunt(self):
+        print(self.__name, "hunt", self.__hunt)
+
+    def animal_fight(self):
+        print(self.__name, "other", self.__fight)
+
+    
+
+
+   
+    
 
